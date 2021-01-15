4164091063a393407a3000d622da999637e3dbb7
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-01-15 13:29:14 -0600

    added time-blocks with appropriate classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,5 @@
 // get current day for scheduler title
 var dayOfWeek = moment().format("dddd");
 var date = moment().format("MMMM Do");
+// display date on screen
 $("#currentDay").html(dayOfWeek + ", " + date);
