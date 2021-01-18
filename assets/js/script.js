5f0b2920e59ca9db4251a1d55a16d8b77120be80
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-01-17 22:27:50 -0600

    added tasks to local storage and ability to load tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// Variables
-
-
+var taskSchedule = [];
 // get current day for scheduler title
 var dayOfWeek = moment().format("dddd");
 var currentDate = moment().format("MMMM Do");
@@ -21,28 +19,90 @@ var auditTimeBlock = function (timeBlockEl) {
     // if current moment is after timeBlock ID set to past
     if (moment().isAfter(timeCompare)) {
         // Add the class of past to the task box.
-        $(timeBlockEl).children(".task-box").addClass("past");
+        $(timeBlockEl).children("textarea").addClass("past");
         // If there are any lingering classes from other times, remove them.
-        $(timeBlockEl).children(".task-box").removeClass("present future");
+        $(timeBlockEl).children("textarea").removeClass("present future");
     }
     // if current moment is equal to timeBlock ID set to present
     else if (moment().isSame(timeCompare)) {
-        $(timeBlockEl).children(".task-box").addClass("present");
-        $(timeBlockEl).children(".task-box").removeClass("past future");
+        $(timeBlockEl).children("textarea").addClass("present");
+        $(timeBlockEl).children("textarea").removeClass("past future");
     }
 
     // if current moment is before the timeBlock ID set to future
     else if (moment().isBefore(timeCompare)) {
-        $(timeBlockEl).children(".task-box").addClass("future");
-        $(timeBlockEl).children(".task-box").removeClass("past present");
+        $(timeBlockEl).children("textarea").addClass("future");
+        $(timeBlockEl).children("textarea").removeClass("past present");
     }
 };
 
 // Check the time every five minutes to ensure task box coloring is accurate.
-setInterval(function(){
-    $(".time-block").each(function(index, el){
-        auditTimeBlock(el);
+var scheduleSet = function () {
+    $(".time-block").each(function (index, el) {
+        auditTimeBlock(el)
     });
-    },(1000 * 60) * 5);
+};
 
+setInterval(scheduleSet(), (1000 * 60) * 5);
+
+
+var saveTimeTask = function () {
+    localStorage.setItem("schedule", JSON.stringify(taskSchedule));
+};
+
+$("textarea").on("change", function () {
+    var text = $(this)
+        .val()
+        .trim();
+
+    var id = $(this)
+        .closest(".time-block")
+        .attr("id");
+
+    $(".time-block").on("blur", function () {
+        alert("please click the save button");
+    });
+
+    $(".fa-save").on("click", function () {
+        if ($(this).closest(".time-block").attr("id") === id) {
+            var timeTask = {
+                task: text,
+                time: id
+            };
+
+            taskSchedule.push(timeTask);
+
+            saveTimeTask();
+        }
+    });
+
+});
+
+var loadSchedule = function () {
+    taskSchedule = JSON.parse(localStorage.getItem("schedule"));
+  
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!taskSchedule) {
+        taskSchedule = [];
+    }
+  
+    // loop over object properties
+    $.each(taskSchedule, function (timeBlock , text) {
+      console.log(timeBlock, text);
+
+      var id = text.time;
+      id = "#" + id.toString();
+
+      var item = text.task
+      item = item.toString();
+
+      $(id).children("textarea").val(item);
+        
+      //});
+    });
+  };
+
+
+scheduleSet();
+loadSchedule();
 
